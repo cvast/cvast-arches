85370b4b0992d10de4dd8b5cdefe705e5c07ca0f
Vincent <meijer.vincent@gmail.com>
2017-09-29 13:43:32 -0400

    DEV-275 Move actionsToolbar inside js module, make it global using window.actionsTOolbar. Format document

--- cvast_arches/cvast_arches/media/js/reports/cvast-3dhop.js
+++ cvast_arches/cvast_arches/media/js/reports/cvast-3dhop.js
@@ -1,56 +1,55 @@
-function actionsToolbar(action) {
-	if (action == 'home') presenter.resetTrackball();
-	else if (action == 'zoomin') presenter.zoomIn();
-	else if (action == 'zoomout') presenter.zoomOut();
-	else if (action == 'light' || action == 'light_on') { presenter.enableLightTrackball(!presenter.isLightTrackballEnabled()); lightSwitch(); }
-	else if (action == 'sections' || action == 'sections_on') { sectiontoolReset(); sectiontoolSwitch(); }
-	else if (action == 'full' || action == 'full_on') fullscreenSwitch();
-}
 
-
-define([    
-	'spidergl',
-	'jquery',
-	'presenter',
-	'nexus',
-	'trackball_sphere',
-	'trackball_turntable',
-	'trackball_turntable_pan',
-	'trackball_pantilt',
-    'ply',	
-	'init',
+define([
+    'spidergl',
+    'jquery',
+    'presenter',
+    'nexus',
+    'trackball_sphere',
+    'trackball_turntable',
+    'trackball_turntable_pan',
+    'trackball_pantilt',
+    'ply',
+    'init',
 ], function (spidergl, $, presenter, nexus, trackball_sphere, trackball_turntable, trackball_turntable_pan, trackball_pantilt, ply, init) {
 
-	
+    window.actionsToolbar = function actionsToolbar(action) {
+        if (action == 'home') presenter.resetTrackball();
+        else if (action == 'zoomin') presenter.zoomIn();
+        else if (action == 'zoomout') presenter.zoomOut();
+        else if (action == 'light' || action == 'light_on') { presenter.enableLightTrackball(!presenter.isLightTrackballEnabled()); lightSwitch(); }
+        else if (action == 'sections' || action == 'sections_on') { sectiontoolReset(); sectiontoolSwitch(); }
+        else if (action == 'full' || action == 'full_on') fullscreenSwitch();
+    }
 
-	return {
 
-		setupURL: function (source) {
+    return {
 
-			presenter = new Presenter("draw-canvas");
-			
-			presenter.setScene({
-				meshes: {
-					// The path in which the model to be loaded in the viewer is stored
-					"mesh1": { url: source }
-				},
-				modelInstances: {
-					"Inst1": {
-						mesh: "mesh1"
-					}
-				},
-				trackball: {
-					type: SphereTrackball,
-					trackOptions: {
-						startDistance: 2.5,
-						minMaxDist: [0.01, 5.0]
-					}
-				},
-				space: {
-					centerMode: "scene",
-					radiusMode: "scene"
-				}
-			});
-		}
-	}
+        setupURL: function (source) {
+
+            presenter = new Presenter("draw-canvas");
+
+            presenter.setScene({
+                meshes: {
+                    // The path in which the model to be loaded in the viewer is stored
+                    "mesh1": { url: source }
+                },
+                modelInstances: {
+                    "Inst1": {
+                        mesh: "mesh1"
+                    }
+                },
+                trackball: {
+                    type: SphereTrackball,
+                    trackOptions: {
+                        startDistance: 2.5,
+                        minMaxDist: [0.01, 5.0]
+                    }
+                },
+                space: {
+                    centerMode: "scene",
+                    radiusMode: "scene"
+                }
+            });
+        }
+    }
 });
